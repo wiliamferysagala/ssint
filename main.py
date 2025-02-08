8ea5669f631304470e0bfab74532d2d646d26483
wiliamferysagala <wiliamferysagala@gmail.com>
2025-02-09 05:56:00 +0700

    h-1

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 a = np.array([1, 2, 3])
 b = np.array([4, 5, 6])
 
 print(a + b)
+
+names = ['Alice', 'Bob', 'Charlie']
+
+user = [{'name': name, 'age': np.random.randint(20, 30)} for name in names]
+
+print(user)
+
+
+vektor = np.array([1, 2, 3])
+print("vektor:\n", vektor)
+
+matriks =  np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print("matriks:\n", matriks)
+plt.imshow(matriks, cmap='hot')
+plt.colorbar()
+plt.show()
