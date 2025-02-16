dfefaa9ac185a1ba66f905f3fca5434aa8704e0b
wiliamferysagala <wiliamferysagala@gmail.com>
2025-02-16 22:32:59 +0700

    +feat:framer-motion

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -1,18 +1,33 @@
 import { cn } from "@/lib/util";
-import React from "react";
+import { ReactNode } from "react";
+import { motion, useScroll, useSpring } from "motion/react";
 
-const Base = ({ children }: { children: React.ReactNode }) => {
+const Base = ({ children }: { children: ReactNode }) => {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
+  console.log(scrollYProgress);
+  console.log(scaleX);
+
+  const classVal = [
+    "min-h-[150vh] w-full",
+    "bg-[#0088ff]",
+    "text-white font-bold text-2xl",
+    "relative",
+  ];
+
   return (
-    <div
-      className={cn(
-        "min-h-screen w-full",
-        "flex justify-center items-center",
-        "bg-[#0088ff]",
-        "text-white font-bold text-2xl"
-      )}
-    >
-      {children}
-    </div>
+    <motion.div className={cn(classVal)}>
+      <motion.div
+        style={{ scaleX }}
+        className="w-full bg-red-500 fixed top-0 h-1 left-0"
+      />
+      <main>{children}</main>
+    </motion.div>
   );
 };
 
